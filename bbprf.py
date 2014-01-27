265405c50a1c1eca2c1ffcaaf1a84a2804a4fca7
Lars Kiesow <lkiesow@uos.de>
2014-01-27 18:14:46 +0100

    Added assigned reviewer to table
    This commit introduces an additional column containing the reviewer of a
    certain pull request. To do this it is necessary to have a list of pull
    request identifier and assigned reviewers which has to be created and
    maintained manually.
    
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- bbprf.py
+++ bbprf.py
@@ -1,4 +1,11 @@
 #!/bin/env python
+# -*- coding: utf-8 -*-
+
+# Set default encoding to UTF-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 import urllib2
 import json
 from pprint import pprint
@@ -6,7 +13,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
+
+	reviewers = {}
+	try:
+		with open('reviewers', 'r') as f:
+			for line in f.read().split('\n'):
+				try:
+					issue, reviewer = line.split('=', 1)
+					reviewers[int(issue)] = reviewer
+				except:
+					pass
+	except IOError:
+		pass
 
 	requests = []
 
@@ -22,7 +41,10 @@ def hello():
 		finally:
 			u.close()
 
-	pprint(requests[0])
+	# Add reviewer
+	for req in requests:
+		req[u'reviewer'] = reviewers.get(req['id'])
+
 	return render_template('home.html', requests=requests)
 	#return '%s' % [ pr.get('title') + '\n' for pr in requests ]
 
