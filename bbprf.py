ce3e587c164f3a0164069e2ebfc616c3d86fce3c
Lars Kiesow <lkiesow@uos.de>
2014-01-29 13:41:53 +0100

    Try to get reviewer from comments
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- bbprf.py
+++ bbprf.py
@@ -12,8 +12,34 @@ from pprint import pprint
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+last_updated = {}
+
+
+def get_reviewer(id):
+	url = 'https://bitbucket.org/api/2.0/repositories/opencast-community/matterhorn/pullrequests/%s/comments' % id
+
+	u = urllib2.urlopen(urllib2.Request(url))
+	try:
+		data = json.loads(u.read())
+	finally:
+		u.close()
+
+	for c in data.get('values') or []:
+		if '//review//' in c['content']['raw']:
+			reviewer = c['user']['display_name']
+			print 'Found reviewer for %s: %s' % (id, reviewer)
+			with open('reviewers', 'a') as f:
+				f.write('%s=%s\n' % (id, reviewer))
+			return reviewer
+
+	print 'No reviewer found for %s', id
+	return None
+
+
 @app.route("/")
 def home():
+	global last_updated
 
 	reviewers = {}
 	try:
@@ -43,7 +69,14 @@ def home():
 
 	# Add reviewer
 	for req in requests:
-		req[u'reviewer'] = reviewers.get(req['id']) or ''
+		if reviewers.get(req['id']):
+			req[u'reviewer'] = reviewers.get(req['id'])
+		else:
+			if req.get('updated_on') != last_updated.get(req['id']):
+				req[u'reviewer'] = get_reviewer(req['id']) or ''
+			if not req.get(u'reviewer'):
+				req[u'reviewer'] = ''
+				last_updated[req['id']] = req.get('updated_on')
 
 	# Sort by id
 	requests.sort(key=lambda r: r.get('id'))
